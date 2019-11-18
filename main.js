d3bcc4cc0813a3785790f6ec2b8d5583e40d1d53
Renzo Carara <renzo.carara@libero.it>
2019-11-18 23:16:41 +0100

    inseriti maggiori controlli sulla stringa in input + stile piccola modifica

--- main.js
+++ main.js
@@ -13,24 +13,49 @@ var FamilyNames = ["ROCKERDUCK",
     "BASETTONI",
     "MANETTA",
     "MOUSE"
-] // array di stringhe alfabetiche non ordinate
+]; // array di stringhe alfabetiche non ordinate
 var surname = ""; // cognome che inserisce l'utente
+var surnameMinLen = 1; // minima lunghezza ammessa per il cognome introdotto dall'utente
 var surnameMaxLen = 20; // massima lunghezza ammessa per il cognome introdotto dall'utente
-var surnameValid = false; // validità della stringa cognome introdotta dall'utente
+var surnameValid; // validità della stringa cognome introdotta dall'utente
 var posizione = 0; // posizione dell'elemento inserito nell'array
+
+
 
 //recupero l'input dall'utente, mi attendo una stringa alfabetica (cognome)
 do {
-    surname = prompt("Inserisci il tuo Cognome (max 20 caratteri alfabetici)");
-    //controlli minimi su correttezza input
-    // non deve essere un numero e deve essere massimo surnameMaxLen caratteri
-    // ATTENZIONE: con questo controllo, stringhe alfanumeriche (tipo "aa33" o "45ggg" vengono accettate)
-    if ((isNaN(surname)) && (surname.length <= surnameMaxLen)) {
-        surnameValid = true;
-    } else {
-        alert("ATTENZIONE: input non valido. \nInserisci una stringa alfabetica di max 20 caratteri. \nGrazie.")
+    surnameValid = true; // all'inizio assumo che la stringa di input sia valida
+    surname = prompt("Inserisci il tuo Cognome (min 1 - max 20 caratteri alfabetici)");
+    // controlli minimi su correttezza input
+    // la stringa non deve essere nulla, non devono esserci numeri nella stringa,
+    // deve essere massimo surnameMaxLen caratteri e minimo surnameMinLen caratteri
+    // rimane da valutare:
+    // - inserimento di caratteri speciali...($,&,-,etc.)
+    // - inserimento 1 o più spazi iniziali
+    // - stringa tipo: "xx    xxxx", cioè con più spazi tra 2 stringhe di caratteri
+
+    // controllo che l'input è nullo o maggiore di surnameMaxLen o  minore di surnameMinLen caratteri
+    if ((surname == null) || (surname.length > surnameMaxLen) || (surname.length < surnameMinLen)) {
+        surnameValid = false;
+    } else { // lunghezza OK, ora verifico se ci sono dei numeri mischiati ai caratteri
+        for (var i = 0;
+            (i < surname.length); i++) {
+            // console.log("surname[", i, "]: ", surname[i]);
+            // la 1a condizione verifica se ci sono dei numeri, la 2a condizione è inserita perchè
+            // isNan di " " ritorna false
+            if (!isNaN(surname[i]) && (surname[i] != " ")) {
+                surnameValid = false;
+                // console.log("trovato numero");
+                // console.log("surnameValid: ", surnameValid);
+            }
+        }
     }
-} while (!surnameValid);
+
+    if (!surnameValid) {
+        alert("ATTENZIONE: input non valido. \nInserisci una stringa alfabetica da 1 a max 20 caratteri. \nGrazie.");
+    }
+}
+while (!surnameValid); // finchè l'input non è valido continuo a ciclare e richiedere un input valido
 
 // -- UPPERCASE lowercase --
 // trasformo la stringa di input nel formato UPPERCASE (nel caso non lo fosse),
@@ -53,20 +78,24 @@ for (var i = 0; i < FamilyNames.length; i++) {
 }
 
 // visualizzo array ordinato
-// METODO 1
+// SOLUZIONE 1
 // for (var i = 0; i < FamilyNames.length; i++) {
 //     document.getElementById("cognomi").innerHTML += FamilyNames[i] + "<br>";
 // }
 
-// METODO 2
+// SOLUZIONE 2
 // document.getElementById("cognomi").innerHTML = FamilyNames;
 
-// METODO 3
+// SOLUZIONE 3
 for (var i = 0; i < FamilyNames.length; i++) {
-    node = document.createElement("p"); // creo un nuovo <p>
+    node = document.createElement("p"); // creo un nuovo tag <p> per ogni elemento da visualizzare
     node.innerHTML = FamilyNames[i]; // valorizzo iL nuovo elemento
     document.getElementById("cognomi").appendChild(node); // lo inserisco nella giusta posizione sulla pagina
+    if (i == posizione - 1) {
+        // coloro il nuovo cognome inserito nella lista
+        node.setAttribute("class", "cyano");
+    }
 }
 
-// visualizzo la posizione del cognome appena inserito
+// visualizzo il numrino che indica la posizione del cognome appena inserito
 document.getElementById("posizione").innerHTML = posizione;
